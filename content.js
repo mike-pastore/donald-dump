35424e15fda5c0a3f51504e87c542da7d3c217a3
Mike Pastore <mike.t.pastore@gmail.com>
2016-01-01 23:45:05 -0600

    Add random array of 'ump' words

--- content.js
+++ content.js
@@ -1,8 +1,22 @@
+var names = [
+	'Chump',
+	'Clump',
+	'Dump',
+	'Flump',
+	'Grump',
+	'Lump',
+	'Thump'
+];
+
+function chooseRandomName() {
+	return names[Math.floor(Math.random() * names.length)];
+}
+
 chrome.runtime.onMessage.addListener(
 	function (request, sender, sendResponse) {
 		if (request.message === "clicked_browser_action") {
 			var replaced = $("body").html().replace(/Trump|trump/g, function (x) {
-				return 'Dump';
+				return chooseRandomName();
 			});
 			$("body").html(replaced);
 		}
