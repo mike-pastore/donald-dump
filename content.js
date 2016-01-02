6fc1f404d8cc58ab58d7a26b434d1f6721ff26d6
Mike Pastore <mike.t.pastore@gmail.com>
2016-01-01 22:54:02 -0600

    Add lowercase 'trump' support

--- content.js
+++ content.js
@@ -1,7 +1,9 @@
 chrome.runtime.onMessage.addListener(
 	function (request, sender, sendResponse) {
 		if (request.message === "clicked_browser_action") {
-			var replaced = $("body").html().replace(/Trump/g, 'Dump');
+			var replaced = $("body").html().replace(/Trump|trump/g, function (x) {
+				return 'Dump';
+			});
 			$("body").html(replaced);
 		}
 	}
