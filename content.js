23973822af26142cf8d3915d831fd3e87b0e29d5
Mike Pastore <mike.t.pastore@gmail.com>
2016-02-04 22:03:49 -0600

    Replace all images on page with cats from lorempixel.com

--- content.js
+++ content.js
@@ -15,6 +15,17 @@ function chooseRandomName() {
 chrome.runtime.onMessage.addListener(
 	function (request, sender, sendResponse) {
 		if (request.message === "clicked_browser_action") {
+			var images = document.images.length;
+
+			for (i = 0; i < images; i++) {
+				console.log("image #" + i + " src = " + document.images[i].src);
+				var width = document.images[i].width;
+				var height = document.images[i].height;
+
+				document.images[i].src = "http://lorempixel.com/" + width + "/" + height + "/cats/? " 
+				+ new Date().toString() + "";
+			}
+
 			var replaced = $("body").html().replace(/Trump|trump/g, function (x) {
 				return chooseRandomName();
 			});
